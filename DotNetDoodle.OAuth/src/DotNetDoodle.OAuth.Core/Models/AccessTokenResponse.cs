4c7fa13dcfbafd54947e7bd551e8830fce563a14
Tugberk Ugurlu <tugberkugurlu@gmail.com>
2013-11-06 18:11:12 +0200

    done a few more set up work on the handler

--- DotNetDoodle.OAuth/src/DotNetDoodle.OAuth.Core/Models/AccessTokenResponse.cs
+++ DotNetDoodle.OAuth/src/DotNetDoodle.OAuth.Core/Models/AccessTokenResponse.cs
@@ -1,4 +1,5 @@
-﻿using System.Runtime.Serialization;
+﻿using System;
+using System.Runtime.Serialization;
 
 namespace DotNetDoodle.OAuth.Core.Models
 {
@@ -12,9 +13,27 @@ namespace DotNetDoodle.OAuth.Core.Models
         public string TokenType { get; set; }
 
         [DataMember(Name = "expires_in")]
-        public uint ExpiresIn { get; set; }
+        public double ExpiresIn { get; set; }
 
         [DataMember(Name = "refresh_token")]
         public string RefreshToken { get; set; }
+
+        public bool IsExprired
+        {
+            get
+            {
+                DateTime expiresInDateTime = UnixTimeStampToDateTime(ExpiresIn);
+                return DateTime.Now > expiresInDateTime;
+            }
+        }
+
+        // http://stackoverflow.com/questions/249760/how-to-convert-unix-timestamp-to-datetime-and-vice-versa
+        private static DateTime UnixTimeStampToDateTime(double unixTimeStamp)
+        {
+            // Unix timestamp is seconds past epoch.
+            System.DateTime dtDateTime = new DateTime(1970, 1, 1, 0, 0, 0, 0);
+            dtDateTime = dtDateTime.AddSeconds(unixTimeStamp).ToLocalTime();
+            return dtDateTime;
+        }
     }
 }
--- DotNetDoodle.OAuth/src/DotNetDoodle.OAuth.Core/OAuthBearerHandler.cs
+++ DotNetDoodle.OAuth/src/DotNetDoodle.OAuth.Core/OAuthBearerHandler.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using DotNetDoodle.OAuth.Core.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Net.Http;
@@ -11,32 +12,52 @@ namespace DotNetDoodle.OAuth.Core
     public class OAuthBearerHandler : DelegatingHandler
     {
         // http://stackoverflow.com/questions/17801995/c-sharp-async-await-limit-of-calls-to-async-methods-locking
-        private SemaphoreSlim _syncLock = new SemaphoreSlim(1);
+        private readonly SemaphoreSlim _syncLock = new SemaphoreSlim(1);
+        private readonly Func<HttpRequestMessage, Task<AccessTokenResponse>> _accessTokenFunc;
+        private AccessTokenResponse _accessTokenToUse;
 
-        public OAuthBearerHandler()
+        public OAuthBearerHandler(Func<HttpRequestMessage, Task<AccessTokenResponse>> accessTokenFunc)
         {
+            _accessTokenFunc = accessTokenFunc;
         }
 
         protected override async Task<HttpResponseMessage> SendAsync(HttpRequestMessage request, CancellationToken cancellationToken)
         {
+            if (_accessTokenToUse == null || _accessTokenToUse.IsExprired)
+            {
+                try
+                {
+                }
+                catch
+                {
+                    throw;
+                }
+            }
+
             // TODO: 1-) Check to see if we have an available bearer token to send with this request.
             //           1.1-) if we have, send it through the Authorization: Bearer {token} and jump to 3.
             //           1.2-) if not, continue with 2.
             //       2-) As we don't have a bearer token available, prepare for the Token request. Not sure for know which flow should we apply here.
             //           2-1-) After getting the token from the server, add it to its known place with the refresh token if available.
 
+            return await base.SendAsync(request, cancellationToken);
+        }
+
+        private async Task<AccessTokenResponse> RetrieveAccessTokenAsync(HttpRequestMessage request)
+        {
             try
             {
                 // Only 1 thread can access the function or functions that use this lock, 
                 // others trying to access - will wait until the first one released.
                 await _syncLock.WaitAsync();
+                AccessTokenResponse accessToken = await _accessTokenFunc(request);
+
+                return accessToken;
             }
             finally
             {
                 _syncLock.Release();
             }
-
-            return await base.SendAsync(request, cancellationToken);
         }
     }
 }
