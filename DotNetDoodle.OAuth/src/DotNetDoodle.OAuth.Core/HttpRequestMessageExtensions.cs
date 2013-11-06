ae7e9b10a1c36891fcfa2b088e6d248db600b222
Tugberk Ugurlu <tugberkugurlu@gmail.com>
2013-11-06 17:30:08 +0200

    started working on the OAuthBearerHandler

---
+++ DotNetDoodle.OAuth/src/DotNetDoodle.OAuth.Core/HttpRequestMessageExtensions.cs
@@ -0,0 +1,13 @@
+﻿using System.Net.Http;
+using System.Net.Http.Headers;
+
+namespace DotNetDoodle.OAuth.Core
+{
+    public static class HttpRequestMessageExtensions
+    {
+        public static void SetBearerToken(this HttpRequestMessage request, string token)
+        {
+            request.Headers.Authorization = new AuthenticationHeaderValue("Bearer", token);
+        }
+    }
+}
---
+++ DotNetDoodle.OAuth/src/DotNetDoodle.OAuth.Core/OAuthBearerHandler.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace DotNetDoodle.OAuth.Core
+{
+    public class OAuthBearerHandler : DelegatingHandler
+    {
+        public OAuthBearerHandler()
+        {
+        }
+
+        protected override Task<HttpResponseMessage> SendAsync(HttpRequestMessage request, CancellationToken cancellationToken)
+        {
+            // TODO: 1-) Check to see if we have an available bearer token to send with this request.
+            //           1.1-) if we have, send it through the Authorization: Bearer {token} and jump to 3.
+            //           1.2-) if not, continue with 2.
+            //       2-) As we don't have a bearer token available, prepare for the Token request. Not sure for know which flow should we apply here.
+            //           2-1-) After getting the token from the server, add it to its known place with the refresh token if available.
+
+            return base.SendAsync(request, cancellationToken);
+        }
+    }
+}
